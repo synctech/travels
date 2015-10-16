fa89aa02f0d100bba7c01ff4f5733e68d86de8f8
synctech <jp21.wayne@gmail.com>
2015-10-17 00:06:33 +0800

    Happy Travel Website

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace backend\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace backend\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
